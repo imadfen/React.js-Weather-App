4fc8458e3cd20b34b0ee96af34b8d9bc34a50e3b
imadfen <imadfen20@gmail.com>
2023-08-02 21:38:11 +0100

    fixing enviroment variables error

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -1,19 +1,21 @@
 import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_METEO_API_URL;
+const apiKey = import.meta.env.VITE_METEO_API_KEY;
+const fetchUrl = `${apiUrl}&key=${apiKey}&q=`
 
 const axiosInstance = axios.create();
 export default async (location: string | null, userPosition?: { latitude: number, longitude: number } | null) => {
     try {
         if (location) {
-            return await axiosInstance.get(apiUrl + location)
+            return await axiosInstance.get(fetchUrl + location)
         } else {
             location = "london"
             if (userPosition) {
                 location = `${userPosition.latitude},${userPosition.longitude}`
             }
             localStorage.setItem("wheather-app-location", location)
-            return await axiosInstance.get(apiUrl + location)
+            return await axiosInstance.get(fetchUrl + location)
         }
     } catch (error) {
         throw new Error("Error fetching data from the API");
